31ef7735de9862d0a5d09e574a001ee067dc3587
Myhanhpham25 <myhanhpham25@gmail.com>
2018-05-21 14:36:46 -0700

    update navbar

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,12 +7,9 @@ const Header = () => (
     <header>
         <h1>Expensify</h1>
         <NavLink to="/" activeClassName="is-active" exact={true}>Dashboard</NavLink>
-        <NavLink to="/create" activeClassName="is-active">Create</NavLink>
-      
-        <NavLink to="/help" activeClassName="is-active">Help</NavLink>
+        <NavLink to="/create" activeClassName="is-active">Create Expense</NavLink>
     </header>
 );
 
 export default Header;
-
 // <NavLink to="/edit" activeClassName="is-active">Edit Expense</NavLink>
