0f615f5739005de89ee1fe330788310d571e080a
Myhanhpham25 <myhanhpham25@gmail.com>
2018-05-29 11:32:21 -0700

    add expense Summary to dashboard

--- src/tests/fixtures/expenses.js
+++ src/tests/fixtures/expenses.js
@@ -4,19 +4,19 @@ export default [{
     id: '1',
     description : 'Gum',
     note : '',
-    amount : 1.95,
+    amount : 195,
     createdAt : 0
 }, {
     id: '2',
     description : 'Rent',
     note : '',
-    amount : 195000,
+    amount : 109500,
     createdAt : moment(0).subtract(4, 'days').valueOf()
 },{
     id: '3',
     description : 'credit card',
     note : '',
-    amount : 45000,
+    amount : 4500,
     createdAt : moment(0).add(4, 'days').valueOf()
 }]
 
